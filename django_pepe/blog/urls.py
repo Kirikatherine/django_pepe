d2e54313631500cf08e7a0abfd722d64f5b3f9cc
Kirikatherine <katya.arty@gmail.com>
2022-10-20 17:06:59 +0300

    first

---
+++ django_pepe/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .views import get_all_topics, get_topics_by_category
+
+
+urlpatterns = [
+    path('all_topics', get_all_topics, name='all_topics'),
+    path('topics_by_category/<str:category>', get_topics_by_category,
+     name='topics_by_category')
+]
+
+
---
+++ django_pepe/blog/views.py
@@ -0,0 +1,24 @@
+from pydoc_data.topics import topics
+from django.shortcuts import render
+from .models import Topic, Category
+from django.contrib.auth.models import User
+
+def get_all_topics(request):
+    all_topics = Topic.objects.all()
+    allowed_viewer = User.object.get(pk=1)
+    greetings = [
+        'hello',
+        'welcome', 
+    ]
+    simple_html_el = "<h1>Zagolovok</h1>"
+    return render(request, 'blog/all_topics.html', {'all_topics': all_topics})
+
+
+def get_topics_by_category(request, category):
+    try: #electronics
+        asked_category = Category.objects.get(title=category)
+        topics_by_category = Topic.objects.filter(category=asked_category)
+        return render(request, 'blog/get_topics_by_category.html', 
+        {'topics_by_category': topics_by_category})
+    except: #aboba
+        return render(request, 'blog/wrong_category.html')
